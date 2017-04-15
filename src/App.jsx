3a4c604422c19ea782e46959537078895e35833e
Shubhangi <shubhangi@teewe.in>
2017-04-15 20:14:59 +0530

    adding redux dependecy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import SearchBar from './containers/SearchBar';
-import './App.css';
 
 export default class App extends Component {
   render() {
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,5 @@
+// Not needed
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
